06bb5e6cf56a8f26e6767af0d4b6d227477785c8
Gabriel Godinho <gabrielgodinho@alunos.utfpr.edu.br>
2023-10-06 09:30:20 -0300

    Initial Commit

---
+++ Nano4/Nano4App.swift
@@ -0,0 +1,17 @@
+//
+//  Nano4App.swift
+//  Nano4
+//
+//  Created by Gabriel Henrique Kwiatkovski Godinho on 06/10/23.
+//
+
+import SwiftUI
+
+@main
+struct Nano4App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
